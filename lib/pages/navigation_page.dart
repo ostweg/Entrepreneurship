399381e818ae3fdbadedb6c770a1541a271c5839
ostweg <hoechstesetablissement@gmail.com>
2023-06-23 20:59:28 +0200

    Added search bar to navigation page

--- lib/pages/navigation_page.dart
+++ lib/pages/navigation_page.dart
@@ -15,34 +15,47 @@ class _NavigationPageState extends State<NavigationPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-            target: LatLng(
-              47.36667,
-              8.55,
-            ),
-            zoom: 11),
-        onMapCreated: (controller) {
-          mapController = controller;
-          addMarker('test', LatLng(47.36667, 8.55));
-        },
-        markers:markers.values.toSet(),
-      ),
+      body: Container(
+          width: double.maxFinite,
+          height: double.maxFinite,
+          child: Stack(
+            children: [
+              GoogleMap(
+                initialCameraPosition: CameraPosition(
+                    target: LatLng(
+                      47.36667,
+                      8.55,
+                    ),
+                    zoom: 11),
+                onMapCreated: (controller) {
+                  mapController = controller;
+                  addMarker('test', LatLng(47.36667, 8.55));
+                },
+                markers: markers.values.toSet(),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20, right: 20,top:20),
+                child: TextField(
+                  decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(),
+                      hintText: 'Search by name'),
+                ),
+              )
+            ],
+          )),
     );
   }
-  addMarker(String id, LatLng location){
+
+  addMarker(String id, LatLng location) {
     var marker = Marker(
         markerId: MarkerId(id),
         position: location,
         infoWindow: InfoWindow(
-            title: 'Hahaha',
-            snippet: 'This is my last resort, suffocation'
-        )
-    );
+            title: 'Hahaha', snippet: 'This is my last resort, suffocation'));
 
     markers[id] = marker;
-    setState(() {
-
-    });
+    setState(() {});
   }
 }
